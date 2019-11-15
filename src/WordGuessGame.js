ebfae8fbd2d0d456c0154c4d64a599a20e54123f
Luc van Kerkvoort <41163851+lucvankerkvoort@users.noreply.github.com>
2019-11-15 13:31:26 -0800

    Word guess game
    created the basic game. I still have to work on the guessedLetters and guessesleft

--- src/WordGuessGame.js
+++ src/WordGuessGame.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { bands } from "./artists";
+import { object } from "prop-types";
 
 class WordGuessGame extends React.Component {
   state = {
@@ -7,8 +9,43 @@ class WordGuessGame extends React.Component {
     guessedLetters: [],
     blanks: []
   };
+  guessed = [];
+  componentDidMount() {
+    document.addEventListener("keydown", this.game);
+    const options = Object.keys(bands).splice("");
+    const randomWord = options[Math.floor(Math.random() * options.length)];
+    const selectedWord = randomWord.toLowerCase().split("");
+
+    let blankWord = [];
+    for (let i = 0; i < selectedWord.length; i++) {
+      blankWord.push("-");
+    }
+
+    this.setState({ answer: selectedWord, blanks: blankWord });
+  }
+
+  game = e => {
+    const { answer } = this.state;
+    let input = e.key;
+    let correctLetters = this.state.blanks;
+    for (let i = 0; i < answer.length; i++) {
+      if (input === answer[i]) {
+        correctLetters[i] = answer[i];
+      }
+    }
+    this.guessed.push(input);
+    console.log("this.guessed", this.guessed);
+    this.setState({ blanks: correctLetters, guessedLetters: this.guessed });
+  };
   render() {
-    return <h1>Hello World</h1>;
+    console.log("answer", this.state.answer);
+    return (
+      <div>
+        <h1>{this.state.blanks}</h1>
+        <h1>Guesses Left: {this.state.guessesLeft}</h1>
+        <h1>Guessed Letters: {this.state.guessedLetters}</h1>
+      </div>
+    );
   }
 }
 
